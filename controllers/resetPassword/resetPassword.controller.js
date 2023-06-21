29dd69d28fa2c8431e1187829f5ecd0165fc714a
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-22 02:22:21 +0400

    change reset password email notification template

--- controllers/resetPassword/resetPassword.controller.js
+++ controllers/resetPassword/resetPassword.controller.js
@@ -49,7 +49,7 @@ const resetUserPassword = async (req, res) => {
             from: 'Nicolas@example.com',
             to: 'ggdnicolas@gmail.com',
             subject: 'Subject',
-            text: resetLink
+            html: resetPasswordTemplate(resetLink)
         };
 
         nodeTransporter.sendMail(mailOptions, (error, info) => {
@@ -142,6 +142,37 @@ const findUserWithToken = async (req, res) => {
         return res.status(400).json(error);
     }
 };
+
+const resetPasswordTemplate = (reset_link) => {
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Email Verification</title>
+      </head>
+      <body>
+        <table align="center" width="600" cellpadding="0" cellspacing="0" style="background-color: #fff; border-collapse: collapse;">
+          <tr>
+            <td style="padding: 20px;">
+              <h1 style="text-align:center;">Welcome to ${process.env.COMPANY_NAME}</h1>
+              <p>Thanks for updating profile.</p>
+              <table align="center" style="margin: 20px auto;">
+                <tr>
+                  <td>
+                    <a href="${reset_link}" style="background-color: #49d2d2; color: white; padding: 12px 20px; border: none; border-radius: 4px; cursor: pointer; font-size: 18px; text-decoration: none;">
+                      Reset password
+                    </a>
+                  </td>
+                </tr>
+              </table>
+              <p>If you have any issues or didn't sign up for an account, please contact us at <a href="mailto:${process.env.COMPANY_EMAIL}">${process.env.COMPANY_EMAIL}</a></p>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>  
+      `;
+}
 
 module.exports = {
     resetUserPassword,
