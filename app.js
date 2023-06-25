7c6d9d242a41c52cda9a009d515112fce9fa8473
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-26 00:15:32 +0400

    create contact route

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const userAuth = require('./controllers/auth/auth.routes');
 const userLogOut = require('./controllers/logout/logout.routes');
 const resetPassword = require('./controllers/resetPassword/resetPassword.routes');
 const userController = require('./controllers/user/user.routes');
+const contactMe = require('./controllers/contact/contact.routes');
 
 const app = express();
 dotenv.config();
@@ -28,6 +29,7 @@ app.use('/auth', userAuth);
 app.use('/logout', userLogOut);
 app.use('/reset-password', resetPassword);
 app.use('/user', userController);
+app.use('/contact', contactMe);
 
 app.use(verifyJWT);
 app.use(notFound);
