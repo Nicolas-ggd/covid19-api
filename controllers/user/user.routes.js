e313a619c736fe27d01250c38f474bf84e62259c
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-16 17:15:45 +0400

    update

--- controllers/user/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('./user.controller.js');
-
-router.get('/user-list', userController.getUserList);
-
-module.exports = router;
