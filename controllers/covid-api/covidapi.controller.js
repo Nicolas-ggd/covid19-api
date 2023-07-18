86ef7c3d0be31238083701528998a7c162ef5b03
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-18 17:59:13 +0400

    update

--- controllers/covid-api/covidapi.controller.js
+++ controllers/covid-api/covidapi.controller.js
@@ -4,18 +4,27 @@ const getCovidData = async (req, res) => {
     const queryData = req.query;
     const page = queryData.page ? parseInt(queryData.page) : 1;
     const limit = queryData.limit ? parseInt(queryData.limit) : 7;
-
+    const search = queryData.search || '';
+  
     try {
-        const response = await axios.get(`https://corona.lmao.ninja/v2/countries?page=${page}&limit=${limit}`);
-        const data = response.data;
-        const startIdx = (page - 1) * limit;
-        const endIdx = startIdx + limit;
+      const response = await axios.get(
+        `https://corona.lmao.ninja/v2/countries?page=${page}&limit=${limit}`
+      );
+      const data = response.data;
 
-        const chunkData = data.slice(startIdx, endIdx);
-
-        return res.status(200).json(chunkData);
+      const filteredData = data.filter((item) =>
+        item.country.toLowerCase().includes(search.toLowerCase())
+      );
+  
+      const startIdx = (page - 1) * limit;
+      const endIdx = startIdx + limit;
+  
+      const chunkData = filteredData.slice(startIdx, endIdx);
+  
+      return res.status(200).json(chunkData);
     } catch (error) {
-        return res.status(400).json({ message: "Can't get covid data." });
+      console.log(error);
+      return res.status(400).json({ message: "Can't get covid data." });
     }
 };
 
