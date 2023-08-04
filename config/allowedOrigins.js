f2393e96d2072a52ea35d02a94cd4fdc135da005
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-04 13:33:16 +0400

    create build script to build app in deploy time && update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:5173',
+    'https://covit-api.netlify.app/',
     'http://localhost:8000/refresh',
     'http://localhost:8000/login',
     'https://corona.lmao.ninja/v2/countries'
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDb = require('./config/db');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const cookieParser = require('cookie-parser');
+const corsOptions = require('./config/corsOptions');
+const credentials = require('./middleware/credentials');
+const verifyJWT = require('./middleware/verifyJWT');
+const register = require('./controllers/register/register.routes');
+const userAuth = require('./controllers/auth/auth.routes');
+const userLogOut = require('./controllers/logout/logout.routes');
+const resetPassword = require('./controllers/resetPassword/resetPassword.routes');
+const covidApi = require('./controllers/covid-api/covidapi.routes');
+
+const app = express();
+dotenv.config();
+connectDb();
+
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.options('*', cors())
+app.use('/register', register);
+app.use('/auth', userAuth);
+app.use('/logout', userLogOut);
+app.use('/reset-password', resetPassword);
+app.use('/covid', covidApi);
+
+app.use(verifyJWT);
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`));
