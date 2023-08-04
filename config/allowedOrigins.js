6f42ae4f12f6c185fc0804cad6a12303d108ed79
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-04 13:42:26 +0400

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
     'https://covit-api.netlify.app/',
     'http://localhost:8000/refresh',
     'http://localhost:8000/login',
+    'https://covid19-api-rj18.onrender.com/login',
+    'https://covid19-api-rj18.onrender.com/refresh',
     'https://corona.lmao.ninja/v2/countries'
 ];
 
